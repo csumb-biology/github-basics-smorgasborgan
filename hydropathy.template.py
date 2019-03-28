1d1e3b18e4963e23d5aebdae34188a4706f1f6db
ericcrandall <eric.d.crandall@gmail.com>
2019-03-28 14:03:42 -0700

    add hydropathy script and human protein dataset

---
+++ hydropathy.template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+#Working template of hydropathy score calculation script
+#You need to put in comments for every line
+
+InFileName = "amino_acid_hydropathy_values.txt"
+InFile = open(InFileName, 'r')
+Data=[]
+Hydropathy={}
+LineNumber = 0
+
+for Line in InFile:
+    if(LineNumber>0):
+        Line = Line.strip("\n")
+        Data = Line.split(",")
+        Hydropathy[Data[1]]=float(Data[2])
+    LineNumber = LineNumber + 1
+InFile.close()
+
+window = raw_input("Window size?")
+window=int(window)
+Value=0
+window_counter=0
+InSeqFileName = raw_input("Name of sequence file to analyze?\n")
+InSeqFile = open(InSeqFileName, 'r')
+LineNumber = 0
+
+for Line in InSeqFile:
+    if(LineNumber>0):
+        ProtSeq=Line.strip('\n')
+    LineNumber = LineNumber + 1
+InSeqFile.close()
+
+OutFileName = InSeqFileName.strip('.fasta') + ".output.csv"
+OutFile = open(OutFileName,"w")
+HpValues = []
+for i in range(len(ProtSeq)):
+    Value+=Hydropathy[ProtSeq[i]]
+    if(i>(window-1) and i<=(len(ProtSeq)-window)):
+        Value=Value-Hydropathy[ProtSeq[i-window]]
+        OutString = "%d,%.2f" % (window_counter, Value)
+        OutFile.write(OutString + "\n")
+		HpValues.append(Value)
+    window_counter+=1
+
+OutFile.close()
